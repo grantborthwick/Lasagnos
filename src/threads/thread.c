ff0b7b7f704fc25f65c1221b683f51d61bf95924
Grant Borthwick <grantborthwick@gmail.com>
2012-10-04 00:17:39 -0500

    test2

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -256,7 +256,7 @@ thread_unblock (struct thread *t)
   struct list_elem* u;
   struct thread* e2;
   struct thread* u2 = NULL;
-  /*printf("start\n");
+  printf("start\n");
   for (e = (list_begin (&ready_list)); e!= list_end (&ready_list); 
 	e = list_next(e))
 	{
@@ -268,7 +268,7 @@ thread_unblock (struct thread *t)
 		}
 		printf("%s - %d\n",u2->name,u2->priority);
 	}
-    printf("%s has highest with %d\n\n",u2->name,u2->priority);*/
+    printf("%s has highest with %d\n\n",u2->name,u2->priority);
   
   intr_set_level (old_level);
   printf("leaving unblock.\n");
