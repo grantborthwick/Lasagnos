a04b2daa1217af36a46a0f08c4881e7cb040d4f9
Grant Borthwick <grantborthwick@gmail.com>
2012-10-03 23:11:58 -0500

    test1

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -504,14 +504,13 @@ next_thread_to_run (void)
     else{
 		//printf("Starting!\n");
 		struct thread * e;
-		struct thread * t;
+		struct thread * t = idle_thread;
 		for (e = list_begin (&ready_list); e!= list_end (&ready_list); e = list_next (e)){
-			if (t==NULL || (e->priority)>(t->priority)){t = e;}
+			if ((e->priority)>(t->priority)){t = e;}
 		}
 		//printf("e(%s) has priority %d\n",e->name,e->priority);
 		//list_remove(t);
 		//return t;
-		printf("d\n");
 		return list_entry (list_pop_front (&ready_list), struct thread, elem);
 	}
 			
