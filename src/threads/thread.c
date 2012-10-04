a1fd3494eae53b60ab3e1cb1f0214a521f24ca25
Grant Borthwick <grantborthwick@gmail.com>
2012-10-03 22:18:26 -0500

    test1

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -249,7 +249,7 @@ thread_unblock (struct thread *t)
   ASSERT (t->status == THREAD_BLOCKED);
   list_push_back (&ready_list, &t->elem);
   t->status = THREAD_READY;
-  printf("(%s - %d, %s - %d)\n",(thread_current ()->name), (thread_current ()->priority),(t->name),(t->priority))
+  printf("(%s - %d, %s - %d)\n",(thread_current ()->name), (thread_current ()->priority),(t->name),(t->priority));
   if ((thread_current ()->priority) < (t->priority)){printf(" => Should yield.\n")); /*thread_yield();*/}
   else{printf(" => Did not yield\n");}
   intr_set_level (old_level)
