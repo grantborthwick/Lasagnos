f00f8f6d201f3c64bbc061565aef370ad53c0a0b
Grant Borthwick <grantborthwick@gmail.com>
2012-10-04 01:07:30 -0500

    test2

--- src/tests/threads/alarm-negative.c
+++ src/tests/threads/alarm-negative.c
@@ -1,15 +1,51 @@
-/* Tests timer_sleep(-100).  Only requirement is that it not crash. */
+/* Checks that when the alarm clock wakes up threads, the
+   higher-priority threads run first. */
 
 #include <stdio.h>
 #include "tests/threads/tests.h"
+#include "threads/init.h"
 #include "threads/malloc.h"
 #include "threads/synch.h"
 #include "threads/thread.h"
 #include "devices/timer.h"
 
+static thread_func alarm_priority_thread;
+static int64_t wake_time;
+static struct semaphore wait_sema;
+
 void
-test_alarm_negative (void) 
+test_alarm_priority (void) 
 {
-  timer_sleep (-100);
-  pass ();
+  int i;
+  
+  /* This test does not work with the MLFQS. */
+  ASSERT (!thread_mlfqs);
+
+  wake_time = timer_ticks () + 5 * TIMER_FREQ;
+  sema_init (&wait_sema, 0);
+  
+  for (i = 0; i < 10; i++) 
+    {
+      int priority = PRI_DEFAULT - (i + 5) % 10 - 1;
+      char name[16];
+      snprintf (name, sizeof name, "priority %d", priority);
+      thread_create (name, priority, alarm_priority_thread, NULL);
+    }
+  printf("starter! (%d)%s\n",(thread_current ()->priority),(thread_current ()->name)); 
+  thread_set_priority (PRI_MIN);
+  printf("I am the lowest thread. (%d)%s\n",(thread_current ()->priority),(thread_current ()->name)); 
+  for (i = 0; i < 10; i++)
+    sema_up (&wait_sema);
+  printf("all accounted for.\n");
+}
+
+static void
+alarm_priority_thread (void *aux UNUSED) 
+{
+  
+  sema_down (&wait_sema);
+  /* Print a message on wake-up. */
+  msg ("Thread %s woke up.", thread_name ());
+
+  sema_up (&wait_sema);
 }
