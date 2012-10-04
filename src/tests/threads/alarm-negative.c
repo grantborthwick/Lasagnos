c5eac2bf1dbe57bd6a3be774224d4bdeb94bf7f7
Grant Borthwick <grantborthwick@gmail.com>
2012-10-04 04:32:13 -0500

    fix 4

--- src/tests/threads/alarm-negative.c
+++ src/tests/threads/alarm-negative.c
@@ -41,7 +41,7 @@ test_alarm_negative (void)
       snprintf (name, sizeof name, "priority %d", priority);
       thread_create (name, priority, alarm_priority_thread, NULL);
     }
-  timer_sleep(100);
+  timer_sleep(1000);
   for (i = 0; i < 10; i++)
     sema_up (&wait_sema);
   printf("all accounted for.\n");
