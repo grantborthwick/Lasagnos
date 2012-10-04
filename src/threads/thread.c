308f93a47d18e3f2a0ed790452cc3cfe8c25aa32
Grant Borthwick <grantborthwick@gmail.com>
2012-10-04 00:26:56 -0500

    test2

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -351,7 +351,7 @@ thread_yield (void)
       cur->status = THREAD_READY;
       schedule ();
       intr_set_level (old_level);
-	  printf("I'm back from the dead.\n");
+	  printf("(%d)%s is back from the dead.\n",(current_thread()->priority),(current_thread()->name));
 }
 
 /* Invoke function 'func' on all threads, passing along 'aux'.
