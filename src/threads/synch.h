221141252334e3d73da09fbb00d4ce477fdb5098
Grant Borthwick <grantborthwick@gmail.com>
2012-10-04 21:00:35 -0500

    messup

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -22,6 +22,7 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
+	struct list_elem* location;
   };
 
 void lock_init (struct lock *);
--- src/threads/thread.h
+++ src/threads/thread.h
@@ -95,6 +95,7 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
     struct list_elem timer_list_elem;
+	struct list locks;
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
