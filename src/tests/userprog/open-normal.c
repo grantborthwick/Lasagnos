92db82f2eb40e09e3d1e7f0bb9093edece6cd9dc
Benjamin Kuestersteffen <bkuester@ksu.edu>
2012-10-27 21:37:35 -0500

    updates

--- src/tests/userprog/open-normal.c
+++ src/tests/userprog/open-normal.c
@@ -1,14 +1,12 @@
 /* Open a file. */
 
 #include <syscall.h>
-#include <stdio.h>
 #include "tests/lib.h"
 #include "tests/main.h"
 
 void
 test_main (void) 
 {
-  printf("Opening Sample.txt");
   int handle = open ("sample.txt");
   if (handle < 2)
     fail ("open() returned %d", handle);
